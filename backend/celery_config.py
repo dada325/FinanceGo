b102e887e8ef2811c04495e22bb12f2c8df480ee
Wenda Zheng <dylan.d.zheng@gmail.com>
2023-08-05 03:47:34 +0200

    Create celery_config.py

---
+++ backend/celery_config.py
@@ -0,0 +1,20 @@
+#
+#Please replace 'pyamqp://guest@localhost//' with your actual RabbitMQ connection string, or with the connection string of your chosen broker.
+#
+
+
+from celery import Celery
+
+app = Celery('tasks', broker='pyamqp://guest@localhost//', backend='rpc://')
+
+app.conf.beat_schedule = {
+    'fetch-every-30-minutes': {
+        'task': 'tasks.fetch_articles',
+        'schedule': 30.0 * 60.0,
+    },
+    'process-every-hour': {
+        'task': 'tasks.process_articles',
+        'schedule': 60.0 * 60.0,
+    },
+}
+app.conf.timezone = 'UTC'
