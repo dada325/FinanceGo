847ade931f30c3e64323236e4d386798b0633eaf
Wenda Zheng <dylan.d.zheng@gmail.com>
2023-08-05 03:25:52 +0200

    Update alpaca_data.py

--- article_inflow/alpaca_data.py
+++ article_inflow/alpaca_data.py
@@ -1,14 +1,13 @@
-import logging
-from database_helpers import get_new_articles, update_article_in_database, mark_as_processed
+from celery_config import app
+from database_helpers import get_new_articles, update_article_in_database, mark_as_processed, add_to_database, is_in_database
 from .gpt4_processing import process_with_gpt4
 import requests
 import os
-
+import logging
 
 logging.basicConfig(level=logging.INFO)
 
 # Fetching Alpaca API credentials from environment variables
-# Here must be change to your own API_KEY on ALPACA
 ALPACA_API_KEY_ID = os.environ.get('ALPACA_API_KEY_ID')
 ALPACA_API_SECRET_KEY = os.environ.get('ALPACA_API_SECRET_KEY')
 
@@ -21,52 +20,34 @@ HEADERS = {
 @app.task
 def fetch_articles():
     try:
-        response = call_alpaca_api()
+        response = requests.get(ALPACA_ENDPOINT, headers=HEADERS)
+        response.raise_for_status()  # This will raise an HTTPError if the HTTP request returned an unsuccessful status code
+
         news_data = response.json()
-        
+
         for article in news_data:
             if not is_in_database(article):
                 add_to_database(article)
                 logging.info(f"Added article {article['id']} to database.")
+
     except requests.exceptions.HTTPError as http_err:
         logging.error(f"HTTP error occurred: {http_err}")
     except Exception as e:
         logging.error(f"An error occurred: {e}")
 
-
-def call_alpaca_api():
-    """
-    Calls the Alpaca API and returns the response.
-    Raises an error for non-2xx responses.
-    """
-    response = requests.get(ALPACA_ENDPOINT, headers=HEADERS)
-    response.raise_for_status()  # This will raise an HTTPError if the HTTP request returned an unsuccessful status code
-    return response
-
-
-# get new article and process with gpt3
-
-def get_new_articles():
-    """
-    Retrieve new articles that haven't been processed yet.
-    """
-    connect_to_database()
-    return articles_collection.find({'processed': False})
-
-
-
-# i choose here to 
 @app.task
 def process_articles():
     new_articles = get_new_articles()
     
     for article in new_articles:
-        # Process the article with GPT-4
-        processed_article = process_with_gpt4(article)
+        try:
+            # Process the article with GPT-4
+            processed_article = process_with_gpt4(article)
 
-        # Update the article in the database with the processed data
-        update_article_in_database(processed_article)
+            # Update the article in the database with the processed data
+            update_article_in_database(processed_article)
 
-        # Mark the article as processed
-        mark_as_processed(article['id'])
-
+            # Mark the article as processed
+            mark_as_processed(article['id'])
+        except Exception as e:
+            logging.error(f"An error occurred while processing article {article['id']}: {e}")
