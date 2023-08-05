3489123365ab797acd9bcd6b1c7c02dfa1ee66d0
Wenda Zheng <dylan.d.zheng@gmail.com>
2023-08-05 03:19:14 +0200

    Create celery_config.py

---
+++ article_inflow/celery_config.py
@@ -0,0 +1,15 @@
+from celery import Celery
+
+app = Celery('tasks', broker='pyamqp://guest@localhost//')
+
+app.conf.beat_schedule = {
+    'fetch-every-30-minutes': {
+        'task': 'tasks.fetch_articles',
+        'schedule': 30.0 * 60.0,
+    },
+    'process-every-hour': {
+        'task': 'tasks.process_articles',
+        'schedule': 60.0 * 60.0,
+    },
+}
+app.conf.timezone = 'UTC'
